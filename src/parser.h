a90b112dc398248df9651a7e4c0ca1c1321f185b
zrottman <zrottman@example.com>
2023-10-17 14:57:57 -0700

    Finish parser implementation with better error reporting in eat

--- src/parser.h
+++ src/parser.h
@@ -1,10 +1,13 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+#include <stdio.h>
 #include "safestring.h"
 #include "lexer.h"
+#include "token.h"
 
 int parse(FILE* fp_in);
 int parse_function(FILE* fp_in, struct token* tok);
+int parse_statement(FILE* fp_in, struct token* tok);
 
 #endif // PARSER_H
